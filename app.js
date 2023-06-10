646333c2f74e5bd29bbf17498d65e1effc3ec633
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2023-06-10 15:05:13 +0530

    uri changed

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
   clientID:process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "https://erin-jittery-kitten.cyclic.app/auth/google/home",
+  callbackURL: "https://article-generator-web.cyclic.app/auth/google/home",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, cb) {
